75d5b73ba5440d5840f23417dc9fd8f53dd9f5de
mxj <2212977665@qq.com>
2023-04-09 11:56:33 +0800

    update

--- lab1/Code/main.c
+++ lab1/Code/main.c
@@ -2,18 +2,18 @@
 
 bool hasError = false;
 extern int yyparse();
-extern void yyrestart(FILE *);
+extern void yyrestart(FILE*);
 extern int yydebug;
 extern void analyseTree();
 
-int main(int argc, char **argv)
+int main(int argc, char* argv[])
 {
     if (argc <= 1)
     {
         yyparse();
         return 1;
     }
-    FILE *f = fopen(argv[1], "r");
+    FILE* f = fopen(argv[1], "r");
     if (!f)
     {
         perror(argv[1]);
