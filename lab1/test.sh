473a56e038e39ef36c3d500b1d32d1bfe097f918
mxj <2212977665@qq.com>
2023-04-12 15:32:49 +0800

    update

--- lab1/test.sh
+++ lab1/test.sh
@@ -2,7 +2,7 @@ echo "--------------------------------" > ../Test/output
 for i in ../Tests/*.cmm
 do 
     echo $i >> ../Test/output;
-    ./parser $i >> ../Test/output;
+    ./Code/parser $i >> ../Test/output;
     if [ $? -ne 0 ]; 
     then
         echo $i;
