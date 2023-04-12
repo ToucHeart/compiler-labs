777c0d23927fa3212f484e1277061cf42eb4fe51
mxj <2212977665@qq.com>
2023-04-12 12:52:03 +0800

    add

---
+++ lab1/Code/test.sh
@@ -0,0 +1,11 @@
+echo "--------------------------------" > ../Test/output
+for i in ../Tests/*.cmm
+do 
+    echo $i >> ../Test/output;
+    ./parser $i >> ../Test/output;
+    if [ $? -ne 0 ]; 
+    then
+        echo $i;
+    fi
+    echo "------------------------------" >> ../Test/output
+done
